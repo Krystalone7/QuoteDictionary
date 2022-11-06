9106049130cc3f45e9a2b202979a51303fbe2279
Ivanov Artyom <kristallix2002@mail.ru>
2022-11-06 10:28:18 +0400

    Исправление аннотации

--- src/main/java/com/artyom/quotedictionary/entities/Quote.java
+++ src/main/java/com/artyom/quotedictionary/entities/Quote.java
@@ -2,10 +2,10 @@ package com.artyom.quotedictionary.entities;
 
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,22 +13,19 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "quotes")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
+@Getter
+@Table(name = "quotes")
 public class Quote {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "text")
     @NotNull
     private String text;
-    @Column(name = "author")
     @NotNull
     private String author;
-
     public Quote(String text, String author) {
         this.text = text;
         this.author = author;
